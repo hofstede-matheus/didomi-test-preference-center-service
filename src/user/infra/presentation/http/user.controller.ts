797cc2bee67bfcf737b17e8394c74ac62c0c46c1
hofstede-matheus <hofstede.matheus@gmail.com>
2025-01-26 22:02:43 +0100

    feat(create-user): implement DomainErrorToHttpExceptionFilter

--- src/user/infra/presentation/http/user.controller.ts
+++ src/user/infra/presentation/http/user.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseFilters } from '@nestjs/common';
 import { CreateUserUseCase } from '../../../application/usecases/CreateUser.usecase';
 import { CreateUserRequest, CreateUserResponse } from './dto/create-user';
+import { DomainErrorToHttpExceptionFilter } from './filters/domain-error-to-http-exception.filter';
 
+@UseFilters(DomainErrorToHttpExceptionFilter)
 @Controller('users')
 export class UserController {
   constructor(private readonly createUserUseCase: CreateUserUseCase) {}
